0b4ebcb50d571555547eacef09262e0a27ba3764
Pawanpathariya <pawanpathariys@gmail.com>
2024-09-03 09:16:41 +0530

    practice oops q2

---
+++ q2.cpp
@@ -0,0 +1,33 @@
+/*.Write a C++ program to find the area of circle using class circle which have following details:
+
+a. Accept radius from the user
+b. Calculate the area
+c. Display the result*/
+
+#include<iostream>
+using namespace std;
+
+class Circle{
+private:
+double radius,area;
+
+public:
+Circle(){
+    cout<<"Enter radius ";
+    cin>>radius;
+}
+
+void cal_area(){
+ area=3.14*radius*radius;
+}
+
+void display(){
+    cout<<"Area of circle is "<<area<<endl;
+}
+};
+
+int main(){
+Circle obj1;
+obj1.cal_area();
+obj1.display();
+}
