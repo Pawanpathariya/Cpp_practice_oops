af3acf7c5a3f923ba4c35e6fb538d57d75a0b463
Pawanpathariya <pawanpathariys@gmail.com>
2024-10-09 14:04:02 +0530

    fix

--- que3.cpp
+++ que3.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     int arr[]={54,4,2,6,45,56,89};
    int len=sizeof(arr)/sizeof(arr[0]);
-   int find,low=0,high=len-1,mid=mid = low + (high - low) / 2; ;
+   int find,low=0,high=len-1,mid= (high+low )/ 2; 
 
   for(int i=0;i<len;i++){
     for(int j=i;j<len;j++){
@@ -28,22 +28,17 @@ cout<<"\n";
 bool flag=true;
 while (low<=high){
 if(find>arr[mid]){
-if(arr[mid]==find){
+ low = mid + 1; 
+mid=(high+low)/2;
+}
+else if(arr[mid]==find){
     cout<<" number find at index "<<mid;
     flag=false;
     break;
-}
- low = mid + 1; 
-mid=high-low+low;
 }
 else{
-    if(arr[mid]==find){
-    cout<<" number find at index "<<mid;
-    flag=false;
-    break;
-}
 high=mid-1;
-mid=high-low;
+mid=(high+low)/2;
 }
 }
  if(flag){
