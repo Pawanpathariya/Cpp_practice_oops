31bd70d474bf11d000bb29902394d18a96c4cfaf
Pawanpathariya <pawanpathariys@gmail.com>
2025-01-01 21:57:57 +0530

    selection sort

--- DSA/Insertionsort.cpp
+++ DSA/Insertionsort.cpp
@@ -1,26 +1,29 @@
 #include<iostream>
 using namespace std;
-void insrt(int arr[],int s){
-    for(int i=1;i<s;i++){
-        int key=i;
-        while(key>0 && arr[key]<arr[key-1]){
-            int temp=arr[key];
-            arr[key]=arr[key-1];
-            arr[key-1]=temp;
-            key--;
-        }
+void insertion(int arr[],int s){
+for(int i=0;i<s;i++){
+int key=i;
+while(key>0 && arr[key]<arr[key-1]){
+    int t=arr[key];
+    arr[key]=arr[key-1];
+    arr[key-1]=t;
+    key--;
 }
+    }
 }
 int main(){
-    int arr[]={3,1,5,6,2};
+    int arr[]={3,1,2,5,4};
     int s=sizeof(arr)/sizeof(arr[0]);
-cout<<"Before sorting \n";
+    cout<<"Before sorting\n";
     for(int i=0;i<s;i++){
         cout<<arr[i]<<" ";
     }
-insrt(arr,s);
-cout<<"\nAfter sorting \n";
+
+insertion(arr,s);
+
+    cout<<"\nAfter sorting\n";
     for(int i=0;i<s;i++){
         cout<<arr[i]<<" ";
     }
+
 }
